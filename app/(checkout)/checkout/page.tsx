36feb3b557648098c42b552915e56f701e842854
ViktarStar <v.starovoitou@belvg.com>
2024-10-01 20:21:03 +0200

    add default value at checkout page

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,22 +1,25 @@
 import { cookies } from "next/headers";
 import { CART_COOKIE_KEY } from "@/lib/constants";
-import { notFound } from "next/navigation";
 import { db } from "@/db";
-import { CartState } from "@/lib/types/cart";
 import CheckoutWrapper from "@/components/shared/checkout/CheckoutWrapper";
 
 const Page = async () => {
   const cartId = cookies().get(CART_COOKIE_KEY)?.value;
   if (!cartId) {
-    return notFound();
+    return <></>;
   }
-  const cart = (await db.cart.findUnique({
+  const cart = await db.cart.findUnique({
     where: {
       id: cartId,
     },
-  })) as CartState | null;
-  if (!cart) {
-    return notFound();
+    include: {
+      _count: {
+        select: { cartItems: true },
+      },
+    },
+  });
+  if (!cart || !cart?._count?.cartItems) {
+    return <></>;
   }
   return <CheckoutWrapper />;
 };
