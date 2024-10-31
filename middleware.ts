f6f8c14f4031a6fef25635f07e21785687b2b515
Viktar3w <12dodhe21@gmail.com>
2024-10-31 20:57:59 +0100

    add some changing

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,18 @@ import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/onboarding(.*)"]);
 export default clerkMiddleware(async (auth, request) => {
-  const res = NextResponse.next();
-  if (isProtectedRoute(request)) {
-    auth().protect();
-  }
-  const cartId = request.cookies.get(CART_COOKIE_KEY)?.value;
-  if (!cartId) {
-    const token = await getCartToken();
-    if (token !== "") {
-      res.cookies.set(CART_COOKIE_KEY, token, { path: "/", httpOnly: true });
-    }
-  }
-  return res;
+  // const res = NextResponse.next();
+  // if (isProtectedRoute(request)) {
+  //   auth().protect();
+  // }
+  // const cartId = request.cookies.get(CART_COOKIE_KEY)?.value;
+  // if (!cartId) {
+  //   const token = await getCartToken();
+  //   if (token !== "") {
+  //     res.cookies.set(CART_COOKIE_KEY, token, { path: "/", httpOnly: true });
+  //   }
+  // }
+  // return res;
 });
 
 export const config = {
