7281ba7eb941d7d54820f62580d38819e5e447a4
Viktar3w <12dodhe21@gmail.com>
2024-12-17 19:40:59 +0100

    test working without server route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,3 +3,8 @@
 // export const runtime = "edge";
 //
 // export { httpHandler as GET, httpHandler as POST };
+import { NextResponse } from "next/server";
+
+export const POST = () => {
+  return NextResponse.json({ ok: true });
+};
