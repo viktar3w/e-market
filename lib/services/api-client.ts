0119dde9de1dc150f09beb3ae3e6993c67beb3cb
Viktar3w <12dodhe21@gmail.com>
2025-02-13 20:37:39 +0100

    remove not using dependencies

--- lib/services/api-client.ts
+++
@@ -1,8 +0,0 @@
-import * as categories from "@/lib/services/categories";
-import * as components from "@/lib/services/components";
-import * as products from "@/lib/services/products";
-export const API = {
-  products,
-  components,
-  categories
-};
